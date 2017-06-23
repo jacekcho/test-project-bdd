4f13c11938c3ec1f2a51cf9188433bee174571af
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2017-06-23 17:29:29 +0200

    add glue to TestRunner

--- src/test/java/com/automationpractice/steps/TestRunner.java
+++ src/test/java/com/automationpractice/steps/TestRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         monochrome = true,
         plugin = {"pretty", "html:target/cucumber-reports"},
-        features = {"src/test/resources/features/"}
+        features = "src/test/resources/features/",
+        glue = "com.automationpractice.steps"
 )
 @RunWith(Cucumber.class)
 public class TestRunner {
